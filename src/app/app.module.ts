2ca0db4e62b6fbc99eca2adb257fdf25af7a0686
nguyentuananh071103 <nguyenngocbao13579@gmail.com>
2022-01-26 13:57:57 +0700

    bai thi module4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AwesomeListComponent } from './components/awesomes/awesome-list/awesome-list.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { AwesomeCreateComponent } from './components/awesomes/awesome-create/awesome-create.component';
+import { AwesomeUpdateComponent } from './components/awesomes/awesome-update/awesome-update.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AwesomeListComponent,
+    AwesomeCreateComponent,
+    AwesomeUpdateComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
